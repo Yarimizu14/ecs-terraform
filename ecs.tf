cf4e9c40da92597803b03638dfc7aedaf9f19bc5
Kensei Yarimizu <kensei@abeja.asia>
2017-01-16 13:32:16 +0900

    changed file structure

---
+++ ecs.tf
@@ -0,0 +1,22 @@
+resource "aws_ecs_cluster" "simple-cluster" {
+  name = "simple-cluster"
+}
+
+resource "aws_ecs_task_definition" "simple-task" {
+  family = "simple-task"
+  container_definitions = "${file("task-definitions/simple-app-container.json")}"
+}
+
+resource "aws_ecs_service" "simple-service" {
+  name = "simple-service"
+  cluster = "${aws_ecs_cluster.simple-cluster.id}"
+  task_definition = "${aws_ecs_task_definition.simple-task.arn}"
+  iam_role = "ecsServiceRole"
+  desired_count = 1
+
+  load_balancer {
+    elb_name = "${aws_elb.simple-cluster-service-elb.name}"
+    container_name = "simple-app"
+    container_port = 9090
+  }
+}
