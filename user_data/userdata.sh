37fd74b599a3783d23fdad9601cb3786c1a175a8
Kensei Yarimizu <kensei@abeja.asia>
2017-01-16 11:56:40 +0900

    add quay.io setting

--- user_data/userdata.sh
+++ user_data/userdata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo ECS_CLUSTER=simple-cluster >> /etc/ecs/ecs.config
+echo ECS_ENGINE_AUTH_TYPE=docker >> /etc/ecs/ecs.config
+echo ECS_ENGINE_AUTH_DATA='{"https://quay.io":{"username":"<user>","password":"<password>","email":"."}}' >> /etc/ecs/ecs.config
